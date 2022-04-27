93fdc6cf5bbaa1888e37355d4bf6251dee658084
Orin McDonogh <omcdonogh635@gmail.com>
2022-04-27 16:33:53 +0100

    Nematodes Class

---
+++ java/src/ie/tudublin/Nematode.java
@@ -0,0 +1,77 @@
+package ie.tudublin;
+
+import processing.data.TableRow;
+
+public class Nematode {
+    private String name;
+    private int length;
+    private boolean limbs;
+    private String gender;
+    private boolean eyes;
+
+    
+
+    public Nematode(TableRow tr){
+        this(
+            tr.getString("name"),
+            tr.getInt("length"),
+            tr.getInt("limbs") == 1,
+            tr.getString("gender"),
+            tr.getInt("eyes") ==1
+        );
+    }
+
+
+
+    public Nematode(String name, int length, boolean limbs, String gender, boolean eyes) {
+        this.name = name;
+        this.length = length;
+        this.limbs = limbs;
+        this.gender = gender;
+        this.eyes = eyes;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Nematode [eyes=" + eyes + ", gender=" + gender + ", length=" + length + ", limbs=" + limbs + ", name="
+                + name + "]";
+    }
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public float getLength() {
+        return length;
+    }
+    public void setLength(int length) {
+        this.length = length;
+    }
+    public boolean isLimbs() {
+        return limbs;
+    }
+    public void setLimbs(boolean limbs) {
+        this.limbs = limbs;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public boolean isEyes() {
+        return eyes;
+    }
+    public void setEyes(boolean eyes) {
+        this.eyes = eyes;
+    }
+
+    
+
+}
+
+
